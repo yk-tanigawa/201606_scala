11608d9c84efb7052078c0157ad631231b37c3aa
Yosuke TANIGAWA <yk.tanigawa@gmail.com>
2016-06-27 17:53:42 +0900

    add chap. 13. i.e. learned how to use package to manage codes

---
+++ ch13/src/Example.scala
@@ -0,0 +1,14 @@
+package com
+
+class Example {
+  private val hi = "hello"
+}
+
+object Example {
+
+  def main(args: Array[String]) {
+    val obj = new Example
+    println("Hello")
+    println(obj.hi)
+  }
+}
