911915b47a88830ec14c565532711d53acba1b64
Yosuke TANIGAWA <yk.tanigawa@gmail.com>
2016-06-28 14:48:14 +0900

    update Queue in chap.19

--- ch19/Queue.scala
+++ ch19/Queue.scala
@@ -1,27 +1,41 @@
-trait Queue[T] {
+trait Queue[+T] {
   def head: T
   def tail: Queue[T]
-  def enqueue(x: T): Queue[T]
+  def enqueue[U >: T](x: U): Queue[U]
 }
 
 object Queue {
-  def apply[T](xs: T*): Queue[T] =
+  def apply[T](xs: T*) : Queue[T] =
     new QueueImpl[T](xs.toList, Nil)
-  private class QueueImpl[T]  (
-    private val leading: List[T],
-    private val trailing: List[T]
-  ) extends Queue[T] {
-    def mirror =
-      if(leading.isEmpty)
-        new QueueImpl(trailing.reverse, Nil)
-      else
-        this
-    def head: T = mirror.leading.head
-    def tail: QueueImpl[T] = {
-      val q = mirror
-      new QueueImpl(q.leading.tail, q.trailing)
+
+  private class QueueImpl[+T] (
+    private[this] var leading: List[T],
+    private[this] var trailing: List[T]
+  )extends Queue[T] {
+    private[this] def xs = leading ::: trailing.reverse
+
+    override def toString = 
+      xs.mkString("Queue(", ", ", ")")    
+    override def hashCode = xs.hashCode      
+      
+    private def mirror() {
+      if(leading.isEmpty){
+        while(!trailing.isEmpty){
+          leading = trailing.head :: leading
+          trailing = trailing.tail
+        }
+      }
     }
-    def enqueue(x: T) =
-      new QueueImpl(leading, x :: trailing)
+
+    def head: T = {
+      mirror()
+      leading.head
+    }
+    def tail: QueueImpl[T] = {
+      mirror()
+      new QueueImpl(leading.tail, trailing)
+    }
+    def enqueue[U >: T](x: U) =
+      new QueueImpl[U](leading, x :: trailing)
   }
 }
